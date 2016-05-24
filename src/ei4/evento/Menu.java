bd192b8540f1ac4ed1a7221a32b361257d7adfe5
Melchor Vazquez <mel.mvm@gmail.com>
2016-05-24 15:11:59 +0200

    o

--- src/ei4/evento/Menu.java
+++ src/ei4/evento/Menu.java
@@ -1,5 +1,47 @@
 package ei4.evento;
 
-public class Menu {
+import java.util.List;
 
+public class Menu {
+	
+	public List<Comida> comidas;
+	public int votosTotales;
+	public Double presupuestoTotal;
+
+	public Menu(List<Comida> comidas){
+		
+		this.comidas = comidas;
+		
+		for(Comida c: comidas){
+			this.votosTotales += c.getVotos();
+			this.presupuestoTotal += c.getPrecio();		
+		}		
+	}
+	
+	public Menu(){
+		
+		this.comidas = null;
+		this.votosTotales = 0;
+		this.presupuestoTotal = 0.;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Menu con: [votosTotales=" + votosTotales + ", presupuestoTotal=" 
+				+ presupuestoTotal+ "]";
+	}
+
+	public void add(Comida comida, Comida alternativa) {
+		
+		if(!comidas.contains(comida)){
+			if (!comidas.contains(alternativa)){
+				comidas.add(alternativa);
+			}
+			comidas.add(comida);
+		}
+		
+	}
+	
+	
 }
--- src/ei4/evento/pd/ProblemaEventoPD.java
+++ src/ei4/evento/pd/ProblemaEventoPD.java
@@ -1,38 +1,56 @@
 package ei4.evento.pd;
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.Stream;
+
+import com.google.common.base.Preconditions;
 
 import ei4.evento.*;
 import us.lsi.pd.AlgoritmoPD.Sp;
 import us.lsi.pd.ProblemaPD;
 
 public class ProblemaEventoPD implements ProblemaPD<Menu, Comida> {
-	private static ProblemaEventoPD problemaInicial;
+	
 	// propiedad individual -> private static
 	// propiedad compartida -> public static
-	private static Integer presupuesto;
-	private Double valorSolucion = Double.MIN_VALUE;
-	private static List<Comida> comidas;
-	public static Integer numeroDeComidasEnMenu;	
-	private int index;
-
 	
-
-	public static ProblemaEventoPD create(String fichero, Integer c) {
+	//propiedades individuales:
+	private static ProblemaEventoPD problemaInicial;
+	private static Double presupuestoInicial;
+	private Integer votosSolucion;
+	private int index;	
+	
+	//propiedades compartidas:
+	public static List<Comida> comidasDisponibles;
+	public static int numeroDeComidas;
+	
+	public static ProblemaEventoPD create(String fichero, Double presupuesto) {
 		ProblemaEvento.leeComidas(fichero);
-		ProblemaEventoPD.presupuesto = ProblemaEvento.presupuestoTotal;
-		ProblemaEventoPD.problemaInicial = new ProblemaEventoPD();  //TODO: check parameters inside new ProblemaEventoPD
-
-		// TODO: fill this method
-		for(List<Comida> m : ProblemaEvento.comidasPorTipo.values()){
-			ProblemaEventoPD.comidas.addAll(m);
-		}
+		ProblemaEventoPD.presupuestoInicial = presupuesto;
+		ProblemaEventoPD.problemaInicial = new ProblemaEventoPD(0,0.,0);
 		
+		for(List<Comida> m : ProblemaEvento.comidasPorTipo.values()){
+			ProblemaEventoPD.comidasDisponibles.addAll(m);
+		}		
 		
 		return ProblemaEventoPD.problemaInicial;
 	}
 	
-	public ProblemaEventoPD(){
+	public static ProblemaEventoPD create(int i, Double costeAcumulado, Integer votosAcumulados) {
+		ProblemaEventoPD p = new ProblemaEventoPD(i, costeAcumulado, votosAcumulados);
+		return p;
+	}
+	
+
+	private Double costeAcumulado;
+	private Double presupuestoRestante;
+	private Integer votosAcumulados;
+	
+	private ProblemaEventoPD(int index, Double costeAcumulado, Integer votosAcumulados){
+		
+		this.index = index;
+		this.votosAcumulados = votosAcumulados;
+		this.costeAcumulado = costeAcumulado;
+		this.presupuestoRestante = this.presupuestoRestante - costeAcumulado;		
 		
 	}
 	
@@ -43,232 +61,82 @@ public class ProblemaEventoPD implements ProblemaPD<Menu, Comida> {
 	
 	@Override
 	public int size() {
-		return ProblemaEventoPD.comidas.size() - index + 1;
+		return ProblemaEventoPD.comidasDisponibles.size() - index + 1;
 	}
 	
 	@Override
 	public boolean esCasoBase() {
-		// TODO Auto-generated method stub
-		return false;
+		return (this.presupuestoRestante == 0 || 
+				index == ProblemaEventoPD.comidasDisponibles.size() - 1);
 	}
 	
 	@Override
 	public Sp<Comida> getSolucionCasoBase() {
-		// TODO Auto-generated method stub
-		return null;
+		Integer votos = comidasDisponibles.get(index).getVotos();
+		Double num = Math.min(presupuestoRestante/votos, comidasDisponibles.get(index).getPrecio());
+		Comida alternativa = comidasDisponibles.get(index);
+		
+		return Sp.create(alternativa, num);	
 	}
+
 	@Override
 	public Sp<Comida> seleccionaAlternativa(List<Sp<Comida>> ls) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public ProblemaPD<Menu, Comida> getSubProblema(Comida a, int np) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Sp<Comida> combinaSolucionesParciales(Comida a, List<Sp<Comida>> ls) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public List<Comida> getAlternativas() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public int getNumeroSubProblemas(Comida a) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public Menu getSolucionReconstruida(Sp<Comida> sp) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Menu getSolucionReconstruida(Sp<Comida> sp, List<Menu> ls) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Double getObjetivoEstimado(Comida a) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Double getObjetivo() {
-		// TODO Auto-generated method stub
-		return null;
+		Sp<Comida> r = ls.stream().filter(x -> x.propiedad != null).max(Comparator.naturalOrder()).orElse(null);
+		votosSolucion = r.alternativa.getVotos();
+		return r;
 	}
 	
-/*	private static Integer capacidadInicial;
-	private int index;
-
-	public static List<ObjetoMochila> objetos;
-	public static Integer numeroDeObjetos;
-
-	public static ProblemaEventoPD create(String fichero, Integer c) {
-		ProblemaEvento.leeObjetosDisponibles(fichero);
-		ProblemaEventoPD.objetos = ProblemaEvento.getObjetosDisponibles();
-		ProblemaEventoPD.numeroDeObjetos = ProblemaEventoPD.objetos.size();
-
-		ProblemaEventoPD.multiplicidadesMaximas = ProblemaEvento.getObjetosDisponibles().stream()
-				.mapToInt(x -> x.getNumMaxDeUnidades()).boxed().collect(Collectors.toList());
-		ProblemaEventoPD.capacidadInicial = c;
-		ProblemaEventoPD.problemaInicial = new ProblemaEventoPD(0, 0, 0.);
-
-		return ProblemaEventoPD.problemaInicial;
-	}
-
-	public static ProblemaEventoPD create(int index, int pesoAcumulado, double valorAcumulado) {
-		ProblemaEventoPD p = new ProblemaEventoPD(index, pesoAcumulado, valorAcumulado);
-		return p;
-	}
-
-	private Integer pesoAcumulado;
-	private Integer capacidadRestante;
-	private Double valorAcumulado;
-
-	private ProblemaEventoPD(int index, int pesoAcumulado, double valorAcumulado) {
-		this.index = index;
-		this.pesoAcumulado = pesoAcumulado;
-		this.capacidadRestante = capacidadInicial - this.pesoAcumulado;
-		this.valorAcumulado = valorAcumulado;
-	}
-
-	private Boolean constraints(Integer a) {
-		return this.pesoAcumulado + a * objetos.get(index).getPeso() <= capacidadInicial;
-	}
-
-
 	@Override
-	public int size() {
-		return ProblemaEventoPD.numeroDeObjetos - index + 1;
+	public ProblemaPD<Menu, Comida> getSubProblema(Comida a, int np) {
+		Preconditions.checkArgument(np == 0);
+		Double costeAcumulado = this.costeAcumulado + a.getPrecio();
+		Integer votosAcumulados = this.votosAcumulados + a.getVotos();
+		return ProblemaEventoPD.create(index + 1, costeAcumulado, votosAcumulados);
 	}
 
 	@Override
-	public List<Integer> getAlternativas() {
-		List<Integer> ls = IntStream.rangeClosed(0, ProblemaEventoPD.multiplicidadesMaximas.get(this.index))
+	public Sp<Comida> combinaSolucionesParciales(Comida a, List<Sp<Comida>> ls) {
+		Sp<Comida> r = ls.get(0);
+		Double votos = a.getVotos() + r.propiedad;
+		return Sp.create(a, votos);
+	}
+	
+	@Override
+	// TODO: dont have a clue...
+	public List<Comida> getAlternativas() {
+		List<Comida> ls = Stream.rangeClosed(0, ProblemaEventoPD.multiplicidadesMaximas.get(this.index))
 				.filter(x -> this.constraints(x)).boxed().collect(Collectors.toList());
 		Collections.reverse(ls);
 		return ls;
 	}
-
+	
 	@Override
-	public boolean esCasoBase() {
-		return this.capacidadRestante == 0 || index == ProblemaEventoPD.numeroDeObjetos - 1;
-	}
-
-	@Override
-	public Sp<Integer> getSolucionCasoBase() {
-		Integer peso = objetos.get(index).getPeso();
-		int num = Math.min(capacidadRestante / peso, objetos.get(index).getNumMaxDeUnidades());
-		Double val = (double) num * objetos.get(index).getValor();
-		valorSolucion = val;
-		return Sp.create(num, val);
-	}
-
-	@Override
-	public ProblemaPD<Multiset<ObjetoMochila>, Integer> getSubProblema(Integer a, int np) {
-		Preconditions.checkArgument(np == 0);
-		Integer pesoAcumulado = this.pesoAcumulado + a * objetos.get(index).getPeso();
-		Double valorAcumulado = this.valorAcumulado + a * objetos.get(index).getValor();
-		return ProblemaEventoPD.create(index + 1, pesoAcumulado, valorAcumulado);
-	}
-
-	@Override
-	public Sp<Integer> combinaSolucionesParciales(Integer a, List<Sp<Integer>> ls) {
-		Sp<Integer> r = ls.get(0);
-		Double valor = a * objetos.get(index).getValor() + r.propiedad;
-		return Sp.create(a, valor);
-	}
-
-	@Override
-	public Sp<Integer> seleccionaAlternativa(List<Sp<Integer>> ls) {
-		Sp<Integer> r = ls.stream().filter(x -> x.propiedad != null).max(Comparator.naturalOrder()).orElse(null);
-		valorSolucion = r != null ? r.propiedad : Double.MIN_VALUE;
-		return r;
-	}
-
-	@Override
-	public int getNumeroSubProblemas(Integer a) {
+	public int getNumeroSubProblemas(Comida a) {
 		return 1;
 	}
-
+	
 	@Override
-	public Multiset<ObjetoMochila> getSolucionReconstruida(Sp<Integer> sp) {
-		Multiset<ObjetoMochila> m = HashMultiset.create();
-		m.add(ProblemaEventoPD.objetos.get(this.index), sp.alternativa);
+	public Menu getSolucionReconstruida(Sp<Comida> sp) {
+		Menu m = new Menu();;
+		m.add(ProblemaEventoPD.comidasDisponibles.get(this.index), sp.alternativa);
 		return m;
 	}
-
+	
 	@Override
-	public Multiset<ObjetoMochila> getSolucionReconstruida(Sp<Integer> sp, List<Multiset<ObjetoMochila>> ls) {
-		Multiset<ObjetoMochila> m = ls.get(0);
-		m.add(ProblemaEventoPD.objetos.get(this.index), sp.alternativa);
+	public Menu getSolucionReconstruida(Sp<Comida> sp, List<Menu> ls) {
+		Menu m = ls.get(0);
+		m.add(ProblemaEventoPD.comidasDisponibles.get(this.index), sp.alternativa);
 		return m;
 	}
-
+	
 	@Override
-	public Double getObjetivoEstimado(Integer a) {
-		return this.valorAcumulado + this.getCotaSuperiorValorEstimado(a);
+	public Double getObjetivoEstimado(Comida a) {
+		return this.votosAcumulados.doubleValue()+a.getVotos();
 	}
-
+	
 	@Override
 	public Double getObjetivo() {
-		return this.valorAcumulado + this.valorSolucion;
+		return this.votosAcumulados.doubleValue();
 	}
-
-	public Integer getCotaSuperiorValorEstimado(Integer a) {
-		Double r = 0.;
-		Double capacidadRestante = (double) (capacidadInicial - pesoAcumulado);
-		Double nu = (double) a;
-		int ind = this.index;
-		while (true) {
-			r = r + nu * objetos.get(ind).getValor();
-			capacidadRestante = capacidadRestante - nu * objetos.get(ind).getPeso();
-			ind++;
-			if (ind >= objetos.size() || capacidadRestante <= 0.)
-				break;
-			nu = Math.min(multiplicidadesMaximas.get(ind), capacidadRestante / objetos.get(ind).getPeso());
-		}
-		return (int) Math.ceil(r);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((capacidadRestante == null) ? 0 : capacidadRestante.hashCode());
-		result = prime * result + index;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof ProblemaEventoPD))
-			return false;
-		ProblemaEventoPD other = (ProblemaEventoPD) obj;
-		if (capacidadRestante == null) {
-			if (other.capacidadRestante != null)
-				return false;
-		} else if (!capacidadRestante.equals(other.capacidadRestante))
-			return false;
-		if (index != other.index)
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "(" + index + ", " + capacidadRestante + ")";
-	}*/
-
-
+	
 }
